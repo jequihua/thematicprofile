2f65532b7261584127450abf2e738c066994c811
Julián Equihua <julian.equihua@gmail.com>
2015-07-13 21:43:02 -0500

    separated functions from TP script

---
+++ profile_functions.R
@@ -0,0 +1,93 @@
+
+# moving average function
+ma <- function(x,n=5){filter(x,rep(1/n,n), sides=2)}
+
+
+# functions to find classes start and end of vegetation classes in tematic raster
+# (transects)
+
+extrans <- function(raster,transect)
+{
+  raster_extraction <- extract(raster,transect)
+  raster_extraction <- matrix(unlist(raster_extraction),
+                              ncol = length(raster_extraction), byrow = FALSE)
+  return(raster_extraction)
+}
+
+from_to <- function(tematic_raster,transect)
+{
+  # extract raster values lying on given transect and transform to array
+  tematic_raster_extraction<-extrans(tematic_raster,transect)
+  
+  # find change points in classes
+  counter <- 0
+  from_list <- list()
+  to_list <- list()
+  from_list[[1]]<-0
+  for (i in 1:(length(tematic_raster_extraction)-1))
+  {
+    if (tematic_raster_extraction[i]!=tematic_raster_extraction[i+1])
+    {
+      counter = counter+1     
+      to_list[[counter]]<-i
+      from_list[[counter+1]]<-i      
+    }
+  }
+  to_list[[counter+1]]<-length(tematic_raster_extraction)
+  
+  # from/to lists to arrays
+  to_list<-matrix(unlist(to_list), ncol = length(to_list), byrow = FALSE)
+  from_list<-matrix(unlist(from_list), ncol = length(from_list), byrow = FALSE)
+  returnlist <- list("fromarray"=from_list,"toarray"=to_list)
+  return(returnlist)
+}
+
+tematic_profile <- function(tematic_vector,dem_vector,from_array,to_array,color_matrix,fillarea=TRUE,
+                            xlab="Distance from origin",ylab="Height",lwd=1,outputname="perfil.pdf",
+                            width=9,height=6)
+{
+  n <- length(to_array)
+  meters <- c(0,(1:(length(dem_vector)-1))*30)
+  pdf(outputname,onefile=TRUE, paper='A4r',width=width,height=height)
+  plot(meters,dem_vector,type="l",col="black",lwd=1,xlab=xlab,ylab=ylab,bty="n")
+  
+  legend(23000,1000, # where legend is placed (coodinates)
+         legend=color_matrix[,1],
+         fill=color_matrix[,3],
+         title="Vegetation and Land Use")
+  flag<-FALSE
+  if (fillarea)
+  {
+    for (i in 1:n)
+    {
+      if(i==1)
+      {
+        interval <- from_array[i]:to_array[i]
+        color <- color_matrix[tematic_vector[1],3]
+        polygon(c(0,meters[interval],meters[interval[length(interval)]]),c(0,dem_vector[interval],0),
+                col=color,border=color)
+      }
+      else
+      {
+        interval <- from_array[i]:to_array[i]
+        color <- color_matrix[tematic_vector[to_array[i]],3]
+        polygon(c(meters[interval[1]],meters[interval],meters[interval[length(interval)]]),
+                c(0,dem_vector[interval],0),col=color,border=color) 
+      }
+    }
+    dev.off()
+    flag=TRUE
+  }
+  else
+  {
+    for (i in 1:n)
+    {
+      interval <- from_array[i]:to_array[i]
+      color <- color_matrix[tematic_vector[to_array[i]],3]
+      lines(meters[interval],dem_vector[interval],lwd=lwd,col=color)
+    }
+    dev.off()
+    flag=TRUE
+  }
+  return(flag)
+}
--- thematic_terrain_profile.R
+++ thematic_terrain_profile.R
@@ -10,7 +10,7 @@ library("raster")
 setwd("D:/Julian/66_mama_dem")
 
 # shapefile
-chamela <- readOGR(".","Chamela_3")
+chamela <- readOGR("/home/julian/Documents/demprofile/1/Chamela_3.shp","Chamela_3")
 
 # dem
 dem <- raster("D:/Julian/66_mama_dem/DEMsMx/resol30m/mdt50kscw1.tif")
@@ -48,52 +48,25 @@ plot(cham_pol_rast)
 plot(trans[1,],col="red",add=TRUE)
 
 ############################################## LOAD DATA
-cropped_dem<-raster("dem_chamela_90_clean.tif")
-cham_pol_rast<-raster("cham_pol_rast.tif")
-trans <- readOGR(".","transecto")
+cropped_dem<-raster("/home/julian/Documents/demprofile/chamela_90m/dem_chamela_90_clean.tif")
+cham_pol_rast<-raster("/home/julian/Documents/demprofile/chamela_90m/cham_pol_rast90.tif")
+rio <- readOGR("/home/julian/Documents/demprofile/transects/perfil_tres.shp","perfil_tres")
 
 ############################################## PROFILE PLOTTING
 
+# extract data
 
-# functions to find classes start and end of vegetation classes in tematic raster
-# (transects)
+ext_dem <- matrix(extract(cropped_dem,rio)[[1]])
+ext_dem[is.na(ext_dem)]<-1
+ext_dem_smooth <- ma(ext_dem,n=21)
 
-extrans <- function(raster,transect)
-{
-  raster_extraction <- extract(raster,transect)
-  raster_extraction <- matrix(unlist(raster_extraction),
-                                      ncol = length(raster_extraction), byrow = FALSE)
-  return(raster_extraction)
-}
+ext_pol <- matrix(extract(cham_pol_rast,rio)[[1]])
 
-from_to <- function(tematic_raster,transect)
-{
-  # extract raster values lying on given transect and transform to array
-  tematic_raster_extraction<-extrans(tematic_raster,transect)
-                                      
-  # find change points in classes
-  counter <- 0
-  from_list <- list()
-  to_list <- list()
-  from_list[[1]]<-0
-  for (i in 1:(length(tematic_raster_extraction)-1))
-  {
-    if (tematic_raster_extraction[i]!=tematic_raster_extraction[i+1])
-    {
-      counter = counter+1     
-      to_list[[counter]]<-i
-      from_list[[counter+1]]<-i      
-    }
-  }
-  to_list[[counter+1]]<-length(tematic_raster_extraction)
-  
-  # from/to lists to arrays
-  to_list<-matrix(unlist(to_list), ncol = length(to_list), byrow = FALSE)
-  from_list<-matrix(unlist(from_list), ncol = length(from_list), byrow = FALSE)
-  returnlist <- list("fromarray"=from_list,"toarray"=to_list)
-  return(returnlist)
-}
+ext_pol
 
+plot(ext_dem_smooth,type="l")
+
+class(ext_dem)
 
 
 # vegetation profile
@@ -101,85 +74,39 @@ from_to <- function(tematic_raster,transect)
 ####################################################
 ###################################### thematic legend
 color_matrix <- matrix(0,9,3)
-color_matrix[1,1]<-"Agricultura"
-color_matrix[2,1]<-"Asentamiento humano"
-color_matrix[3,1]<-"Bosque de coniferas"
-color_matrix[4,1]<-"Bosque de latifoliadas"
-color_matrix[5,1]<-"Cuerpo de agua"
-color_matrix[6,1]<-"Galeria"
-color_matrix[7,1]<-"Pastizal"
-color_matrix[8,1]<-"Selva"
-color_matrix[9,1]<-"Vegetación hidrofila"
+color_matrix[1,1]<-"Agricultural activities"
+color_matrix[2,1]<-"Human settlements"
+color_matrix[3,1]<-"Pine, Pine-Oak and Mountain mesophyll forests"
+color_matrix[4,1]<-"Oak forest"
+color_matrix[5,1]<-"Water body"
+color_matrix[6,1]<-"Gallery vegetation"
+color_matrix[7,1]<-"Induced and cultivated grasslands"
+color_matrix[8,1]<-"Seasonally dry tropical forest"
+color_matrix[9,1]<-"Aquatic vegetation"
 color_matrix[,2]<-1:9
 color_matrix[1,3]<-colors()[77]
 color_matrix[2,3]<-colors()[84]
 color_matrix[3,3]<-colors()[258]
 color_matrix[4,3]<-colors()[657]
-color_matrix[5,3]<-colors()[461]
+color_matrix[5,3]<-colors()[400]
 color_matrix[6,3]<-colors()[26]
 color_matrix[7,3]<-colors()[652]
 color_matrix[8,3]<-colors()[655]
-color_matrix[9,3]<-colors()[400]
+color_matrix[9,3]<-colors()[474]
 ###################################################
 ###################################################
 
 
-tematic_profile <- function(tematic_vector,dem_vector,from_array,to_array,color_matrix,fillarea=TRUE,
-                            xlab="Distancia sobre transecto (m)",ylab="Altitud",lwd=2,outputname="perfil.pdf",
-                            width=9,height=6)
-{
-  n <- length(to_array)
-  meters <- c(0,(1:(length(dem_vector)-1))*30)
-  pdf(outputname,onefile=TRUE, paper='A4r',width=width,height=height)
-  plot(meters,dem_vector,type="l",col="white",xlab=xlab,ylab=ylab,bty="n")
-  flag<-FALSE
-  if (fillarea)
-  {
-    for (i in 1:n)
-    {
-      if(i==1)
-      {
-        interval <- from_array[i]:to_array[i]
-        color <- color_matrix[tematic_vector[1],3]
-        polygon(c(0,meters[interval],meters[interval[length(interval)]]),c(0,dem_vector[interval],0),
-                col=color,border=color)
-      }
-      else
-      {
-        interval <- from_array[i]:to_array[i]
-        color <- color_matrix[tematic_vector[to_array[i]],3]
-        polygon(c(meters[interval[1]],meters[interval],meters[interval[length(interval)]]),
-                c(0,dem_vector[interval],0),col=color,border=color) 
-      }
-    }
-    dev.off()
-    flag=TRUE
-  }
-  else
-  {
-    for (i in 1:n)
-    {
-      interval <- from_array[i]:to_array[i]
-      color <- color_matrix[tematic_vector[to_array[i]],3]
-      lines(meters[interval],dem_vector[interval],lwd=lwd,col=color)
-    }
-    dev.off()
-    flag=TRUE
-  }
-  return(flag)
-}
 
-# ejemplo rio
-rio2_shp <- readOGR(".","rio2") 
+# Visualize 
 plot(cham_pol_rast)
-plot(rio2_shp,add=TRUE)
+plot(rio,add=TRUE)
 
-arrays <- from_to(cham_pol_rast,rio2_shp)
+arrays <- from_to(cham_pol_rast,rio)
 fromarray <- arrays$fromarray
 toarray <- arrays$toarray
-tematic<-tematic_profile(extrans(cham_pol_rast,rio2_shp),
-                         extrans(cropped_dem,rio2_shp),fromarray,toarray,color_matrix,fillarea=FALSE,
-                         width=14,height=9,outputname="perfil2_90_test_outline.pdf",lwd=1)
+tematic<-tematic_profile(ext_pol,
+                         ext_dem_smooth,fromarray,toarray,color_matrix,fillarea=TRUE,
+                         width=14,height=9,outputname="/home/julian/Documents/demprofile/outputs/perfil_90_smoothed.pdf",
+                         lwd=1)
 
-color=colors()[31]
-plot(1,1,pch=21,bg=color,col=color,cex=2)
